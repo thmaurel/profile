510f8450551e925e048a91115d4f785275b12e82
Thibaud Maurel <maurel.thibaud@free.fr>
2019-12-03 01:11:34 +0100

    new design

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const tabs = document.querySelectorAll('.tab');
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    })
+    tab.classList.add('active');
+  })
+})
